653a50cb95df30ed3647f540005a6bd34895106a
Daniel Di Cesare <dan@pop-os.localdomain>
2020-01-25 15:33:42 -0500

    tons of work

--- led.py
+++ led.py
@@ -3,15 +3,22 @@ from time import sleep
 from signal import pause
 from detection import *
 
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+
+RECYCLE = ["bottle", "tvmonitor"]
+
+
 yesLED = LED(17) #GPIO17, pin 11
 noLED  = LED(18) #GPIO18, pin 12
 servo  = Servo(26) #GPIO37
 
-while True:
-    servo.max()
-    sleep(2)
-    servo.min()
-    sleep(2)
+
+def sorter(objName):
+    if objName in RECYCLE:
+        servo.max()
 
 yesLED.blink()
 pause()
