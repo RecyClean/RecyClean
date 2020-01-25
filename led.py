89eb5f35db18f55c4aeae68cf71bca25f41b126b
Daniel Di Cesare <dan@pop-os.localdomain>
2020-01-25 15:01:02 -0500

    leds!

--- led.py
+++ led.py
@@ -1,1 +1,17 @@
-#yes
+from gpiozero import LED, Servo
+from time import sleep
+from signal import pause
+from detection import *
+
+yesLED = LED(17) #GPIO17, pin 11
+noLED  = LED(18) #GPIO18, pin 12
+servo  = Servo(26) #GPIO37
+
+while True:
+    servo.max()
+    sleep(2)
+    servo.min()
+    sleep(2)
+
+yesLED.blink()
+pause()
